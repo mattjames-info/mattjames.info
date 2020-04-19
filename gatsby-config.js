147b81026a8c6c5991568ed4153947b6e5fc2aab
Matt James <hello@mattjames.info>
2020-04-19 14:48:16 -0400

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,12 @@ module.exports = {
         display: `swap`
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-163843814-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
