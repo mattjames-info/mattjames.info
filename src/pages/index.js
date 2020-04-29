4fb9f9e46cbc4cfdc38d5adeb4f442dddae3f8fb
Matt James <hello@mattjames.info>
2020-04-29 17:05:11 -0400

    updated contacts

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,6 +38,7 @@ const IndexPage = ({data}) => (
       </table>
     </Card>
     <Card cardTitle="contact">
+      <p style={{ marginBottom: `1rem` }}>Email is always best!</p>
       <div style={{ display: `flex`, flexWrap: `wrap`, flexDirection: `row`, width: `100%`, justifyContent: `space-between` }}>
         <div>
           <SocialIcon url="mailto:hello@mattjames.info" bgColor="white" fgColor="blue" target="_blank" rel="noopener noreferrer" />
