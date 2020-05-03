de2e72ec21951b49e2c30df6a40a15d093c8e973
Matt James <hello@mattjames.info>
2020-05-03 16:00:42 -0400

    Remove public/

--- public/component---src-pages-404-js-e75d3e26ccdb8249adff.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Bl7J"),r=a("vrFN"),u=a("JMMV"),s=a("QSOs");t.default=function(){return o.a.createElement(l.a,null,o.a.createElement(r.a,{title:"404: Not found"}),o.a.createElement(s.a,{dir:"404",toHome:!0}),o.a.createElement(u.a,{cardTitle:"404: Not Found!"},o.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-e75d3e26ccdb8249adff.js.map
